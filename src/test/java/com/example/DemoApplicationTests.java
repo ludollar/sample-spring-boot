0d8b5dc2fb2baf77f86085c8e8a6f68e84405bb8
user <ludaaa@hotmail.fr>
2019-09-12 12:03:42 +0200

    ajout test

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertTrue;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,5 +14,34 @@ public class DemoApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+	@Test
+	public void myTest() {
+		assertTrue(true);
+	}
 
+	@Test
+	public void calculate_methods_first_param_should_not_be_null() throws Exception {
+		try {
+			DemoApplication.additionner(null,2);
+			DemoApplication.soustraire(null,2);
+			DemoApplication.multiplier(null,2);
+			assertTrue(false);
+		}
+		catch (Exception e) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void calculate_methods_second_param_should_not_be_null() throws Exception {
+		try {
+			DemoApplication.additionner(1,null);
+			DemoApplication.soustraire(1,null);
+			DemoApplication.multiplier(1,null);
+			assertTrue(false);
+		}
+		catch (Exception e) {
+			assertTrue(true);
+		}
+	}
 }
